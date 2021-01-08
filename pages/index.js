2bf7ae5b6e01e01ed56aa142784bbef97d8edf77
Nick <nick.reynolds@discordapp.com>
2021-01-07 16:31:21 -0800

    add temp favicon

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,10 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>The 147 Representatives Who Voted to Overturn Election Results</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className={styles.main}>
