06c96e0a7b6f1c7caa697fa2e2b25e07e78e8c4d
Nick Reynolds <me@nickreynolds.net>
2021-01-09 18:21:12 -0800

    Use state dropdown instead of auto-calculating

--- components/representativeContent.tsx
+++ components/representativeContent.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { useEffect } from "react";
-import Loader from "./loader";
 import RepresentativeList from "./representativeList";
 import Data, { RepresentativeType } from "../utils/data";
 
@@ -19,37 +17,39 @@ export default function RepresentativeContent() {
   const [typeFilter, setTypeFilter] = useState(
     new Set<RepresentativeType>(filterToSet(""))
   );
-  const [isLoading, setIsLoading] = useState(true);
-  const [stateXX, setStateXX] = useState<string | null>(null);
+  const [stateXX, setStateXX] = useState("");
 
-  useEffect(() => {
-    fetch("/api/location")
-      .then((res) => res.json())
-      .then((location) => {
-        setStateXX(location.region);
-      })
-      .finally(() => setIsLoading(false));
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div className="grid justify-items-center">
-        <Loader />
-      </div>
-    );
-  }
-
-  const allReps = Data.MEMBERS_OF_CONGRESS.filter((rep) =>
-    typeFilter.has(rep.type)
+  const validStates = new Set(
+    Data.MEMBERS_OF_CONGRESS.map((rep) => rep.state_xx)
   );
-  const stateReps = !stateXX
-    ? []
-    : allReps.filter((r) => r.state_xx.toUpperCase() === stateXX.toUpperCase());
+  const reps = Data.MEMBERS_OF_CONGRESS.filter((rep) =>
+    typeFilter.has(rep.type)
+  ).filter(
+    (r) => stateXX === "" || r.state_xx.toUpperCase() === stateXX?.toUpperCase()
+  );
 
   return (
     <>
       <div className="text-l grid justify-items-center">
-        <div>
+        <div className="flex space-x-8">
+          <label className="flex items-center font-bold">
+            State
+            <select
+              className="ml-2 block w-full p-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none"
+              onChange={(e) => setStateXX(e.target.value)}
+            >
+              <option value={undefined}>All States</option>
+              {Data.ALL_STATES.map((state) => (
+                <option
+                  value={state}
+                  key={state}
+                  disabled={!validStates.has(state)}
+                >
+                  {state}
+                </option>
+              ))}
+            </select>
+          </label>
           <label className="flex items-center font-bold">
             Filter
             <select
@@ -57,19 +57,13 @@ export default function RepresentativeContent() {
               onChange={(e) => setTypeFilter(filterToSet(e.target.value))}
             >
               <option value={undefined}>Both</option>
-              <option value={"House"}>House</option>
-              <option value={"Senate"}>Senate</option>
+              <option value={"House"}>Only House</option>
+              <option value={"Senate"}>Only Senate</option>
             </select>
           </label>
         </div>
       </div>
-      {stateXX && stateReps.length > 0 ? (
-        <RepresentativeList
-          reps={stateReps}
-          title={`Representatives in ${stateXX}`}
-        />
-      ) : null}
-      <RepresentativeList reps={allReps} title="All Representatives" />
+      <RepresentativeList reps={reps} />
     </>
   );
 }
