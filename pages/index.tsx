b0d2dc54b81fdedc3a6ddcf5859b9773f761697f
Nick Reynolds <me@nickreynolds.net>
2021-01-11 22:13:01 -0800

    Fix favicon manifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           sizes="16x16"
           href="/favicon/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
 
       <main>
